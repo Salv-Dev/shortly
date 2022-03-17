cfdea7aab59f613fc9ca13e5ad9684b16b629219
Salv-Dev <salv.dev.br@gmail.com>
2022-03-17 20:15:20 -0300

    cria um estilo global para adicionar o reset css, adiciona a biblioteca react-helmet para acessar o head da página e adiciona o link do google fonts no head

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,20 @@
+import Helmet from 'react-helmet';
+import GlobalStyles from './../config/GlobalStyles';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Url Shortening Interface</h1>
-    </div>
+    <>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet" />
+        <title>URL Shortening API</title>
+      </Helmet>
+      <GlobalStyles />
+      <div className="App">
+        <h1>Url Shortening Interface</h1>
+      </div>
+    </>
   );
 }
 
