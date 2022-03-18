ee7e9fb1bd8cd5516c7aa7792589c3cf402656f8
Salv-Dev <salv.dev.br@gmail.com>
2022-03-18 20:19:42 -0300

    estrutura inicial do componente Logo

---
+++ src/components/atoms/Logo/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+const Logo: React.FC = () => {
+  return (
+      <Container></Container>
+  );
+}
+
+export default Logo;
--- src/pages/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('initial text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Url Shortening Interface/i);
-  expect(linkElement).toBeInTheDocument();
-});
