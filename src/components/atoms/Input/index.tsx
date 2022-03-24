ae2b7adc771fcb049a37ba756d94cf3480c95c32
Salv-Dev <salv.dev.br@gmail.com>
2022-03-24 17:36:29 -0300

    adiciona estrutura inicial do componente Input

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,9 @@
+import { Ipt } from './styles';
+
+const Input: React.FC = () => {
+  return (
+      <Ipt></Ipt>
+  );
+}
+
+export default Input;
---
+++ src/components/atoms/Input/styles.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const Ipt = styled.input``;
