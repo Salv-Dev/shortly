56dac79f21633cf7ff286e6a207987edb686e108
Salv-Dev <salv.dev.br@gmail.com>
2022-03-24 17:24:45 -0300

    adiciona a estrutura inicial do UrlShortenerInput

---
+++ src/components/molecules/UrlShortenerInput/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const UrlShortenerInput: React.FC = () => {
+  return (
+      <Container></Container>
+  );
+}
+
+export default UrlShortenerInput;
