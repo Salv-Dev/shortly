cd25b2efa0af0bf729a48af5469cc2258ff46128
Salv-Dev <salv.dev.br@gmail.com>
2022-03-24 17:32:45 -0300

    adiciona no Main e estiliza o background do UrlShortenerInput

--- src/components/molecules/UrlShortenerInput/styles.ts
+++ src/components/molecules/UrlShortenerInput/styles.ts
@@ -1,3 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    height: 150px;
+    margin: 20px;
+    background: url('/images/bg-shorten-mobile.svg') no-repeat;
+    background-position: right top;
+    background-color: ${props => props.theme.palette.common.violet};
+    border-radius: ${props => props.theme.borderRadius};
+`;
