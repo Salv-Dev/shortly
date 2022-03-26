c222fe3a276a936cbfc32b43006ac591d3a5270b
Salv-Dev <salv.dev.br@gmail.com>
2022-03-26 19:54:47 -0300

    estiliza o titulo e a descrição do AdvancedStatistics

--- src/components/molecules/AdvancedStatistics/index.tsx
+++ src/components/molecules/AdvancedStatistics/index.tsx
@@ -1,11 +1,15 @@
 import UrlShortenerInput from '../UrlShortenerInput';
 
-import { Container } from './styles';
+import { Container, WrapperTitle, Title, Desc } from './styles';
 
 const AdvancedStatistics: React.FC = () => {
   return (
       <Container>
           <UrlShortenerInput />
+          <WrapperTitle>
+            <Title>Estatísticas avançadas</Title>
+            <Desc>Acompanhe o desempenho de seus links na web com nosso painel de estatísticas avançadas.</Desc>
+          </WrapperTitle>
       </Container>
   );
 }
--- src/components/molecules/AdvancedStatistics/styles.ts
+++ src/components/molecules/AdvancedStatistics/styles.ts
@@ -3,4 +3,29 @@ import styled from 'styled-components';
 export const Container = styled.section`
     height: 500px;
     background-color: ${props => props.theme.palette.primary.light.background};
+    margin-top: 100px;
+`;
+
+export const WrapperTitle = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    margin: 50px 0 20px 0;
+    padding: 0 20px;
+`;
+
+export const Title = styled.h2`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+    letter-spacing: -1px;
+    line-height: 2.2rem;
+    color: ${props => props.theme.palette.primary.light.main};
+`;
+
+export const Desc = styled.p`
+    font-size: 14px;
+    color: ${props => props.theme.palette.primary.light.text};
+    text-align: center;
+    line-height: 1.4rem;
 `;
