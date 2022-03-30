ab80051fd68d8128a2469f32d98d929eb752010e
Salv-Dev <salv.dev.br@gmail.com>
2022-03-29 23:47:10 -0300

    estiliza e adiciona a logo no Footer

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,8 +1,12 @@
+import Logo from '../../atoms/Logo';
+
 import { Container } from './styles';
 
 const Footer: React.FC = () => {
   return (
-      <Container></Container>
+      <Container>
+          <Logo src="/images/logo.svg" href="/"/>
+      </Container>
   );
 }
 
--- src/components/organisms/Footer/styles.ts
+++ src/components/organisms/Footer/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     height: 800px;
     background-color: ${props => props.theme.palette.primary.light.main};
+    padding: 40px 20px;
 `;
