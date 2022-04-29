95c00a852768d2e299e6e1eb8aa4fc8a2e18ceaa
Salv-Dev <salv.dev.br@gmail.com>
2022-04-28 21:31:54 -0300

    breakpoints footer

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,35 +1,68 @@
+import { useState, useEffect } from 'react';
 import Logo from '../../atoms/Logo';
 import {ReactComponent as LogoSVG} from './../../../assets/images/logo.svg';
 import List from '../../atoms/List';
 import Link from '../../atoms/Link';
 import Icon from '../../atoms/Icon';
 
-import { Container, WrapperList, SocialMedia } from './styles';
+import { Container, WrapperFooter, WrapperList, SocialMedia } from './styles';
 
 
 
 const Footer: React.FC = () => {
+  const [matches, setMatches] = useState(window.matchMedia("(min-width: 1000px)").matches);
+
+  useEffect(() => {
+    window.matchMedia("(min-width: 1000px)")
+    .addEventListener("change", e => {
+      setMatches(e.matches);
+    })
+  }, []);
+
   return (
       <Container>
+        <WrapperFooter>
           <Logo href="/" svg={<LogoSVG />} fill="white" />
-          <WrapperList>
-            <List title="Produtos" style={{ color: 'white' }} align="center">
-              <Link href="/" hoverColor="#2acfcf" ><li>Encurtador de Links</li></Link>
-              <Link href="/" hoverColor="#2acfcf" ><li>Links de Marca</li></Link>
-              <Link href="/" hoverColor="#2acfcf" ><li>Análise</li></Link>
-            </List>
-            <List title="Recursos" style={{ color: 'white' }} align="center">
-              <Link href="/" hoverColor="#2acfcf" ><li>Blog</li></Link>
-              <Link href="/" hoverColor="#2acfcf" ><li>Desenvolvedores</li></Link>
-              <Link href="/" hoverColor="#2acfcf" ><li>Suporte</li></Link>
-            </List>
-            <List title="Companhia" style={{ color: 'white' }} align="center">
-              <Link href="/" hoverColor="#2acfcf" ><li>Sobre</li></Link>
-              <Link href="/" hoverColor="#2acfcf" ><li>Nosso Time</li></Link>
-              <Link href="/" hoverColor="#2acfcf" ><li>Carreira</li></Link>
-              <Link href="/" hoverColor="#2acfcf" ><li>Contato</li></Link>
-            </List>
-          </WrapperList>
+          {matches && 
+            <WrapperList>
+              <List title="Produtos" style={{ color: 'white' }} align="left">
+                <Link href="/" hoverColor="#2acfcf" ><li>Encurtador de Links</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Links de Marca</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Análise</li></Link>
+              </List>
+              <List title="Recursos" style={{ color: 'white' }} align="left">
+                <Link href="/" hoverColor="#2acfcf" ><li>Blog</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Desenvolvedores</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Suporte</li></Link>
+              </List>
+              <List title="Companhia" style={{ color: 'white' }} align="left">
+                <Link href="/" hoverColor="#2acfcf" ><li>Sobre</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Nosso Time</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Carreira</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Contato</li></Link>
+              </List>
+            </WrapperList>
+          }
+          {!matches && 
+            <WrapperList>
+              <List title="Produtos" style={{ color: 'white' }} align="center">
+                <Link href="/" hoverColor="#2acfcf" ><li>Encurtador de Links</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Links de Marca</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Análise</li></Link>
+              </List>
+              <List title="Recursos" style={{ color: 'white' }} align="center">
+                <Link href="/" hoverColor="#2acfcf" ><li>Blog</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Desenvolvedores</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Suporte</li></Link>
+              </List>
+              <List title="Companhia" style={{ color: 'white' }} align="center">
+                <Link href="/" hoverColor="#2acfcf" ><li>Sobre</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Nosso Time</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Carreira</li></Link>
+                <Link href="/" hoverColor="#2acfcf" ><li>Contato</li></Link>
+              </List>
+            </WrapperList>
+          }
           <SocialMedia>
             <Link  hoverColor="#2acfcf">
               <Icon name="facebook" size={24}/>
@@ -44,6 +77,8 @@ const Footer: React.FC = () => {
               <Icon name="instagram" size={24}/>
             </Link>
           </SocialMedia>
+
+        </WrapperFooter>
       </Container>
   );
 }
--- src/components/organisms/Footer/styles.ts
+++ src/components/organisms/Footer/styles.ts
@@ -1,12 +1,37 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
+    background-color: ${props => props.theme.palette.primary.light.main};    
+`;
+
+export const WrapperFooter = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 20px;
-    background-color: ${props => props.theme.palette.primary.light.main};
     padding: 60px 20px;
+
+    @media (min-width: 1000px) {
+        flex-direction: row;
+        align-items: flex-start;
+        padding: 60px 100px;
+        gap: 40px;
+
+        & > a {
+            flex: 1;
+        }
+    }
+
+    @media (min-width: 1200px) {
+        max-width: 900px;
+        margin: 0 auto;
+        padding: 60px 0;
+    }
+
+    @media (min-width: 1400px) {
+        max-width: 1200px;
+        gap: 80px;
+    }
 `;
 
 export const WrapperList = styled.div`
@@ -14,6 +39,15 @@ export const WrapperList = styled.div`
     flex-direction: column;
     gap: 40px;
     margin-top: 40px;
+
+    @media (min-width: 1000px) {
+        flex-direction: row;
+        margin-top: 0;
+    }
+
+    @media (min-width: 1400px) {
+        gap: 80px;
+    }
 `;
 
 export const SocialMedia = styled.div`
@@ -22,4 +56,9 @@ export const SocialMedia = styled.div`
     gap: 20px;
     width: 100%;
     margin-top: 40px;
+
+    @media (min-width: 1000px) {
+        width: auto;
+        margin-top: 0;
+    }
 `;
